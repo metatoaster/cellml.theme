8392e4a7d7bd5d3bdfe65e16f74f380e48513f09
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2009-03-13 04:15:43 +0000

    Applied Gareth's new CellML template into this skin. There are duplicate files, will sort this out later.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/cellml.plone3skin/trunk@3218 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- cellml/plone3skin/browser/viewlets.py
+++ cellml/plone3skin/browser/viewlets.py
@@ -1,5 +1,11 @@
+from zope.component import getMultiAdapter
+
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
+from plone.app.layout.viewlets.common import GlobalSectionsViewlet
+from plone.app.layout.viewlets.common import PersonalBarViewlet
+from plone.app.layout.viewlets.common import PathBarViewlet
 
 # Sample code for a basic viewlet (In order to use it, you'll have to):
 # - Un-comment the following useable piece of code (viewlet python class).
@@ -19,3 +25,53 @@ from plone.app.layout.viewlets.common import ViewletBase
 #
 #    def update(self):
 #        self.computed_value = 'any output'
+
+
+class CellMLLogoViewlet(ViewletBase):
+    index = ViewPageTemplateFile('cellml_logo.pt')
+
+    def update(self):
+        super(CellMLLogoViewlet, self).update()
+
+        self.navigation_root_url = self.portal_state.navigation_root_url()
+
+        portal = self.portal_state.portal()
+        logoName = portal.restrictedTraverse('base_properties').logoName
+        self.logo_tag = portal.restrictedTraverse(logoName).tag()
+
+        self.portal_title = self.portal_state.portal_title()
+
+
+class CellMLSearchBoxViewlet(ViewletBase):
+    """\
+    Can customize this to search through many different things.
+    """
+
+    index = ViewPageTemplateFile('searchbox.pt')
+
+    def update(self):
+        super(CellMLSearchBoxViewlet, self).update()
+
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+
+        props = getToolByName(self.context, 'portal_properties')
+        livesearch = props.site_properties.getProperty('enable_livesearch', False)
+        if livesearch:
+            self.search_input_id = "searchGadget"
+        else:
+            self.search_input_id = ""
+
+        folder = context_state.folder()
+        self.folder_path = '/'.join(folder.getPhysicalPath())
+
+
+class CellMLGlobalSectionsViewlet(GlobalSectionsViewlet):
+    index = ViewPageTemplateFile('sections.pt')
+
+
+class CellMLPersonalBarViewlet(PersonalBarViewlet):
+    pass
+
+class CellMLPathBarViewlet(PathBarViewlet):
+    pass
