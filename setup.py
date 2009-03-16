0e304e01409308ec3d8d3a28b3f05ed93c79d91e
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2009-03-16 04:57:39 +0000

    Preparing to tag version 0.1
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/cellml.theme/trunk@3240 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.0'
+version = open(os.path.join("cellml", "theme", "version.txt")).read(),
 
 setup(name='cellml.theme',
       version=version,
