4f53caaf9ff1dfa5803b908ad19ae2a9e4660248
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-04-15 19:50:35 +1200

    Latest changes from Plone 4.3

--- cellml/theme/skins/cellml_theme_custom_templates/livesearch_reply.py
+++ cellml/theme/skins/cellml_theme_custom_templates/livesearch_reply.py
@@ -11,39 +11,43 @@ from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import PloneMessageFactory as _
 from Products.CMFPlone.browser.navtree import getNavigationRoot
 from Products.CMFPlone.utils import safe_unicode
-from Products.PythonScripts.standard import url_quote
 from Products.PythonScripts.standard import url_quote_plus
+from Products.PythonScripts.standard import html_quote
 
 ploneUtils = getToolByName(context, 'plone_utils')
 portal_url = getToolByName(context, 'portal_url')()
 pretty_title_or_id = ploneUtils.pretty_title_or_id
 plone_view = context.restrictedTraverse('@@plone')
+portal_state = context.restrictedTraverse('@@plone_portal_state')
 
 portalProperties = getToolByName(context, 'portal_properties')
 siteProperties = getattr(portalProperties, 'site_properties', None)
 useViewAction = []
 if siteProperties is not None:
-    useViewAction = siteProperties.getProperty('typesUseViewActionInListings', [])
+    useViewAction = siteProperties.getProperty(
+                        'typesUseViewActionInListings', [])
 
 # SIMPLE CONFIGURATION
 USE_ICON = True
-USE_RANKING = False
-MAX_TITLE = 29
-MAX_DESCRIPTION = 93
+MAX_TITLE = 50
+MAX_DESCRIPTION = 150
 
 # generate a result set for the query
 catalog = context.portal_catalog
 
 friendly_types = ploneUtils.getUserFriendlyTypes()
 
+
 def quotestring(s):
     return '"%s"' % s
+
 
 def quote_bad_chars(s):
     bad_chars = ["(", ")"]
     for char in bad_chars:
         s = s.replace(char, quotestring(char))
     return s
+
 
 # for now we just do a full search to prove a point, this is not the
 # way to do this in the future, we'd use a in-memory probability based
@@ -56,92 +60,125 @@ def quote_bad_chars(s):
 # so we don't even attept to make that right.
 # But we strip these and these so that the catalog does
 # not interpret them as metachars
-##q = re.compile(r'[\*\?\-\+]+').sub(' ', q)
-for char in '?-+*':
+# See http://dev.plone.org/plone/ticket/9422 for an explanation of '\u3000'
+multispace = u'\u3000'.encode('utf-8')
+for char in ('?', '-', '+', '*', multispace):
     q = q.replace(char, ' ')
-r=q.split()
+r = q.split()
 r = " AND ".join(r)
-r = quote_bad_chars(r)+'*'
+r = quote_bad_chars(r) + '*'
 searchterms = url_quote_plus(r)
 
-site_encoding = context.plone_utils.getSiteEncoding()
+params = {'SearchableText': r,
+          'portal_type': friendly_types,
+          'sort_limit': limit + 1}
+
 if path is None:
-    path = getNavigationRoot(context)
-results = catalog(SearchableText=r, portal_type=friendly_types, path=path)
+    # useful for subsides
+    params['path'] = getNavigationRoot(context)
+else:
+    params['path'] = path
 
-searchterm_query = '?searchterm=%s'%url_quote_plus(q)
+# search limit+1 results to know if limit is exceeded
+results = catalog(**params)
 
-RESPONSE = context.REQUEST.RESPONSE
-RESPONSE.setHeader('Content-Type', 'text/xml;charset=%s' % site_encoding)
+searchterm_query = '?searchterm=%s' % url_quote_plus(q)
 
-# replace named entities with their numbered counterparts, in the xml the named ones are not correct
+REQUEST = context.REQUEST
+RESPONSE = REQUEST.RESPONSE
+RESPONSE.setHeader('Content-Type', 'text/xml;charset=utf-8')
+
+# replace named entities with their numbered counterparts, in the xml the named
+# ones are not correct
 #   &darr;      --> &#8595;
 #   &hellip;    --> &#8230;
 legend_livesearch = _('legend_livesearch', default='LiveSearch &#8595;')
-label_no_results_found = _('label_no_results_found', default='No matching results found.')
-label_advanced_search = _('label_advanced_search', default='Advanced Search&#8230;')
-label_show_all = _('label_show_all', default='Show all&#8230;')
-heading_search_results =  _('heading_search_results', default='Search results&#8230;')
+label_no_results_found = _('label_no_results_found',
+                           default='No matching results found.')
+label_advanced_search = _('label_advanced_search',
+                          default='Advanced Search&#8230;')
+label_show_all = _('label_show_all', default='Show all items')
 
 ts = getToolByName(context, 'translation_service')
 
 output = []
+
 
 def write(s):
     output.append(safe_unicode(s))
 
 
 if not results:
-    write('''<h4>%s</h4>''' % ts.translate(heading_search_results))
-    write('''<ul>''')
-    write('''<li>%s</li>''' % ts.translate(label_no_results_found))
-    write('<li><a href="search_form" style="font-weight:normal">%s</a></li>' % ts.translate(label_advanced_search))
-    write('''</ul>''')
+    write('''<fieldset class="livesearchContainer">''')
+    write('''<legend id="livesearchLegend">%s</legend>'''
+            % ts.translate(legend_livesearch, context=REQUEST))
+    write('''<div class="LSIEFix">''')
+    write('''<div id="LSNothingFound">%s</div>'''
+            % ts.translate(label_no_results_found, context=REQUEST))
+    write('''<div class="LSRow">''')
+    write('<a href="%s" class="advancedsearchlink">%s</a>' %
+            (portal_url + '/@@search',
+            ts.translate(label_advanced_search, context=REQUEST)))
+    write('''</div>''')
+    write('''</div>''')
+    write('''</fieldset>''')
 else:
-    write('''<h4>%s</h4>''' % ts.translate(heading_search_results))
-    write('''<ul>''')
+    write('''<fieldset class="livesearchContainer">''')
+    write('''<legend id="livesearchLegend">%s</legend>'''
+            % ts.translate(legend_livesearch, context=REQUEST))
+    write('''<div class="LSIEFix">''')
+    write('''<ul class="LSTable">''')
     for result in results[:limit]:
-
         icon = plone_view.getIcon(result)
         itemUrl = result.getURL()
         if result.portal_type in useViewAction:
             itemUrl += '/view'
+
         itemUrl = itemUrl + searchterm_query
 
-        write('''<li>''')
-        
+        write('''<li class="LSRow">''')
+        write(icon.html_tag() or '')
         full_title = safe_unicode(pretty_title_or_id(result))
         if len(full_title) > MAX_TITLE:
-            display_title = ''.join((full_title[:MAX_TITLE],'...'))
+            display_title = ''.join((full_title[:MAX_TITLE], '...'))
         else:
             display_title = full_title
+
         full_title = full_title.replace('"', '&quot;')
-        write('''<a href="%s" title="%s">''' % (itemUrl, full_title))
-        if icon.url is not None and icon.description is not None:
-            write('''<img src="%s" alt="%s" width="%i" height="%i" />''' % (icon.url,
-                                                                            icon.description,
-                                                                            icon.width,
-                                                                            icon.height))
-        
-        write('''<span class="livesearchResult-title">%s</span>''' % display_title)
-        write('''<span class="livesearchResult-score">[%s%%]</span>''' % result.data_record_normalized_score_)
+        klass = 'contenttype-%s' \
+                    % ploneUtils.normalizeString(result.portal_type)
+        write('''<a href="%s" title="%s" class="%s">%s</a>'''
+                % (itemUrl, full_title, klass, display_title))
         display_description = safe_unicode(result.Description)
         if len(display_description) > MAX_DESCRIPTION:
-            display_description = ''.join((display_description[:MAX_DESCRIPTION],'...'))
+            display_description = ''.join(
+                (display_description[:MAX_DESCRIPTION], '...'))
 
-        if display_description:
-            write('''<span class="livesearchResult-description">%s</span>''' % (display_description))
-        write('''</a>''')
+        # need to quote it, to avoid injection of html containing javascript
+        # and other evil stuff
+        display_description = html_quote(display_description)
+        write('''<div class="LSDescr">%s</div>''' % (display_description))
         write('''</li>''')
         full_title, display_title, display_description = None, None, None
 
-    if len(results)>limit:
+    write('''<li class="LSRow">''')
+    write('<a href="%s" class="advancedsearchlink">%s</a>' %
+            (portal_url + '/@@search',
+            ts.translate(label_advanced_search, context=REQUEST)))
+    write('''</li>''')
+
+    if len(results) > limit:
         # add a more... row
-        write('''<li class="livesearchShowAll">''')
-        write('''<a href="%s">%s</a>''' % ('search?SearchableText=' + searchterms, ts.translate(label_show_all)))
+        write('''<li class="LSRow">''')
+        searchquery = '@@search?SearchableText=%s&path=%s' \
+                        % (searchterms, params['path'])
+        write('<a href="%s" class="advancedsearchlink">%s</a>' % (
+                             searchquery,
+                             ts.translate(label_show_all, context=REQUEST)))
         write('''</li>''')
-        
+
     write('''</ul>''')
+    write('''</div>''')
+    write('''</fieldset>''')
 
-return '\n'.join(output).encode(site_encoding)
-
+return '\n'.join(output).encode('utf-8')
